0efe3a1f6e85d69576ef1fb20c6d12c737b5a4b3
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-20 19:18:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,71 +1,52 @@
-numbers = [25, 7, 1]
-
-def sort_array_asc(array)
-  array.sort
+def sort_array_asc(integers)
+  integers.sort
 end
-
-sort_array_asc([25, 7, 1])
-
-def sort_array_desc(array)
-  array.sort do | left, right|
-  a <=> b
-  end
+def sort_array_desc(integers)
+  integers.sort {|first_num, second_num| second_num <=> first_num}
 end
-sort_array_desc([25, 7, 14])
-
-def sort_array_char_count(array)
-  array.sort do |left, right|
-    left.length <=> right.length
-  end
+def sort_array_char_count(strings)
+  strings.sort {|left, right| left.length <=> right.length}
 end
-sort_array_char_count(["dogs", "cat", "Horses"])
-
-def swap_elements(array)
-  array[1], array[2] = array[2], array[1]
-  array
+def swap_elements(strings)
+  strings[1], strings[2] = strings[2], strings[1]
+  return strings
 end
-swap_elements(["blake", "ashley", "scott"])
-
-def reverse_array(array)
-  array.reverse
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  return array
 end
-
-reverse_array(["blake", "ashley", "scott"])
-
+def reverse_array(integers)
+  new_array = integers.reverse
+  new_array
+end
 def kesha_maker(array)
-  array.each do |item|
-    item[2] = "$"
+  kesha = []
+  array.each do |word|
+    word_array = word.split ""
+    word_array[2] = "$"
+    kesha << word_array.join
   end
+  kesha
 end
-
-kesha_maker(["blake", "ashley", "scott"])
-
 def find_a(array)
   array.select{|string| string.start_with?("a")}
 end
 def sum_array(integers)
   integers.inject{|sum, n| sum + n}
 end
-
-
-def sum_array(array)
-  sum = 0
-  array.each do |num|
-    sum+=num
-  end
-  sum
-end
-
-sum_array([11,4,7,8,9,100,134])
-
 def add_s(array)
-  array.collect do |word|
-    if array[1] == word
-      word
+  array.each_with_index.collect do |string, index|
+    if index == 1
+      string
     else
-      word + "s"
+      string << "s"
     end
   end
 end
 
-add_s(["hand","feet", "knee", "table"])
+
+
+
+
+
+
