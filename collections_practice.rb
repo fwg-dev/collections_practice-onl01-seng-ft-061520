07b02b5a3350be10fa2101f0396409809b2f6a97
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-20 18:54:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,3 +19,10 @@ def sort_array_char_count(array)
   end
 end
 sort_array_char_count(["dogs", "cat", "Horses"])
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+swap_elements(["blake", "ashley", "scott"]) 
