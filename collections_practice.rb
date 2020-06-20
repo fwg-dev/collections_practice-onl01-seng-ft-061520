e730dffee06744751d64bb00ced701f475ebb123
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-20 18:48:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,13 +1,9 @@
 numbers = [25, 7, 1]
 
-def sort_array_asc(numbers)
-  numbers.sort do |a, b|
-   if a == b
-      0
-  elsif a < b
-    -1
-  elsif a > b
-    1
+def sort_array_asc(array)
+  array.sort
 end
 
-end
+sort_array_asc([25, 7, 1])
+
+
